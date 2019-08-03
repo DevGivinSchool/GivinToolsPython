f375818c519e3cd0f288a8fe672cd760a798828a
MinistrBob <ministrbob777@gmail.com>
2019-08-03 13:38:01 +0300

    - Реализована полная разборка писем и формирование объектов Task.

--- gtp/gtp_attendance_book.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-if __name__ == "__main__":
-    csv_path = r"d:\!SAVE\SCV777\3.csv"
-    with open(csv_path, "r", encoding="utf-8") as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            print(" ".join(row))
--- test/any_test.py
+++ test/any_test.py
@@ -1,2 +1,26 @@
-import multiprocessing as mp
-print("Number of processors: ", mp.cpu_count())
+#from Task import *
+#import multiprocessing as mp
+#print("Number of processors: ", mp.cpu_count())
+
+from email.header import decode_header
+
+def get_decoded_str(line):
+    """Decode email field - From.
+    :param line: Raw 7-bit message body input e.g. from imaplib.
+    :return: Body of the letter in the desired encoding
+    """
+    lines = decode_header(line)
+    print(lines)
+    final_text = ""
+    for text, charset in lines:
+        if charset is None:
+            final_text = text.decode()
+        else:
+            final_text = text.decode(str(charset), "ignore")
+    return final_text
+
+line1 = """=?utf-8?b?0K/QvdC00LXQutGBLtCf0LDRgdC/0L7RgNGC?=
+ <noreply@passport.yandex.ru>"""
+
+print(get_decoded_str(line1))
+

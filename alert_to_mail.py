f689bcd34bab1cbbef6be3a2b8c2c14f0f027a33
MinistrBob <ministrbob777@gmail.com>
2019-09-15 00:53:52 +0300

    Вот так письмо отправляется правильно.

--- alert_to_mail.py
+++ alert_to_mail.py
@@ -8,12 +8,20 @@ def send_mail(receiver_email, message, sender_email="robot@givinschool.org", por
     server = smtplib.SMTP_SSL("smtp.yandex.ru", port, context=context)
     server.login(PASSWORDS.logins['ymail_login'], PASSWORDS.logins['ymail_password'])
     server.sendmail(sender_email, receiver_email, message)
+    server.quit()
 
 
 if __name__ == "__main__":
     mail_to = PASSWORDS.logins['admin_emails']
-    msg = """\
-    Subject: Hi there
+    SUBJECT = "Test email from Python"
+    FROM = "robot@givinschool.org"
+    text = "Python 3.4 rules them all!"
 
-    This message is sent from Python."""
+    msg = "\r\n".join((
+        "From: %s" % FROM,
+        "To: %s" % mail_to,
+        "Subject: %s" % SUBJECT,
+        "",
+        text
+    ))
     send_mail(mail_to, msg)

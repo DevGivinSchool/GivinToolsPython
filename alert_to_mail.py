789c400c14c70b2d314ae1dec818ecb9f668d361
MinistrBob <ministrbob777@gmail.com>
2019-09-22 15:56:24 +0300

    Починил отправку писем

--- alert_to_mail.py
+++ alert_to_mail.py
@@ -1,24 +1,34 @@
 import smtplib
 import ssl
+import traceback
+
 import PASSWORDS
 import logging
 
 logger = logging.getLogger('alert_to_mail')
 
 
-def send_mail(receiver_email, message, sender_email="robot@givinschool.org", port=465):
-    logger.info(f"Send email to {receiver_email}")
+def send_mail(receiver_emails, subject, message, sender_email="robot@givinschool.org", port=465):
+    logger.info(f"Send email to {receiver_emails}")
+    print(f"LOGGER = {logger}")
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    server = smtplib.SMTP_SSL("smtp.yandex.ru", port, context=context)
     try:
-        # Create a secure SSL context
-        context = ssl.create_default_context()
-        server = smtplib.SMTP_SSL("smtp.yandex.ru", port, context=context)
         server.login(PASSWORDS.logins['ymail_login'], PASSWORDS.logins['ymail_password'])
-    except Exception as e:
-        logger.info("ERROR: Can't connect to SMTP server")
+    except Exception:
+        logger.error("ERROR: Can't connect to SMTP server:\n" + traceback.format_exc())
     try:
-        server.sendmail(sender_email, receiver_email, message)
-    except Exception as e:
-        logger.info(f"ERROR: Can't send email to {receiver_email}")
+        mail_text = "\r\n".join((
+            "From: %s" % sender_email,
+            "To: %s" % receiver_emails,
+            "Subject: %s" % subject,
+            "",
+            message
+        ))
+        server.sendmail(sender_email, receiver_emails, mail_text)
+    except Exception:
+        logger.error(f"ERROR: Can't send email to {receiver_emails}:\n" + traceback.format_exc())
     server.quit()
 
 
@@ -35,4 +45,4 @@ if __name__ == "__main__":
         "",
         text
     ))
-    send_mail(mail_to, msg)
+    send_mail(FROM, mail_to, msg)

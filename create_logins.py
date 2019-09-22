668350ff05792ed6a2bb28f46b3ebbf68563ccb9
MinistrBob <ministrbob777@gmail.com>
2019-09-22 15:54:03 +0300

    Исправлена процедура создания логинов для ДШ

--- create_logins.py
+++ create_logins.py
@@ -16,7 +16,8 @@ def from_list_create_sf_mails():
         print(line)
         # ['Иванов', 'Иван']
         try:
-            yandex_mail.create_yandex_mail(line[0], line[1], department_id_=4)  # Отдел 4 = @ДРУЗЬЯ_ШКОЛЫ
+            result = yandex_mail.create_yandex_mail(line[0], line[1], department_id_=4)
+            # Отдел 4 = @ДРУЗЬЯ_ШКОЛЫ
         except yandex_connect.YandexConnectExceptionY as e:
             # print(e.args[0])
             if e.args[0] == 500:
@@ -25,7 +26,8 @@ def from_list_create_sf_mails():
             else:
                 print("ERROR = " + e.__str__())
         # Для почты стандартный пароль, это пароль для Zoom
-        print(password_generator.randompassword(strong=True))
+        print(f"Создана почта: {result['email']}")
+        print(f"Пароль для Zoom: {password_generator.random_password(strong=True, zoom=True)}")
 
 
 def create_femaly_mail():
@@ -36,7 +38,7 @@ def create_femaly_mail():
         # При транслитерации некоторые буквы переводятся в - ' - это нужно заменить
         line.append(translit_name(line[0]))
         print(line)
-        password = password_generator.randompassword()
+        password = password_generator.random_password()
         # Отдел 1 = Все сотрудники
         yandex_mail.create_yandex_mail(line[2], line[0], line[1], password, department_id_=1)
         print(password)
@@ -49,7 +51,7 @@ def create_login_mail():
         # Первое слово пойдет в Фамилия остальное в Имя
         line.append(line[0].split(' ', maxsplit=1))
         print(line)  #
-        password = password_generator.randompassword()
+        password = password_generator.random_password()
         print(line[1], line[2][1], line[2][0])  # ['Яцуненко', 'Роман', 'Jatsunenko']
         # Отдел 3 = @СПЕЦПОЧТЫ
         yandex_mail.create_yandex_mail(line[1], line[2][1], line[2][0], password, department_id_=3)
@@ -62,7 +64,7 @@ def create_ftp_login():
     for line in list_fio.splitlines():
         line = line.split(' ')
         print(translit_name(line[0]))
-        print(password_generator.randompassword(strong=True, long=12))
+        print(password_generator.random_password(strong=True, long=12))
 
 
 def show_groups():

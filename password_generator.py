a3d483548eb3fe7b4fd41a4d813c886bf838faa2
MinistrBob <ministrbob777@gmail.com>
2019-10-18 17:59:47 +0300

    Процедура нормализации длины пароля

--- password_generator.py
+++ password_generator.py
@@ -38,24 +38,25 @@ def random_password(strong=False, long=10, zoom=False):
         long = long - 2
     password = ''.join(random.choice(chars) for _ in range(long))
     print(f"password 0={password}")
-    # Пароль должне начинаться с буквы
+    # Пароль должен начинаться с буквы
     if not password[0].isalpha():
         password = random.choice(letters) + password[1:]
     print(f"password 1={password}")
     # Пароль должен содержать хотя бы одну цифру (Zoom), если цифры нет, подставляем на третью позицию случайню цифру
     if not re.search(r'\d', password):
         print(f"цифр нет")
-        password = password[:2] + random.choice(digits_chars) + password[2 + 1:]
+        password = password[:2] + random.choice(digits_chars) + password[2:]
+        password = adjust_password_length(password)
     print(f"password 2={password}")
-    # В сложном пароле должны быть и спецсимволы, если нет,
-    # подставляем на четвёртую позицию
+    # В сложном пароле должны быть и спецсимволы, если нет, подставляем на четвёртую позицию
     chars = set('@#$&', )
     if not any((c in chars) for c in password):
         print(f"спецсимволов нет")
-        password = password[:3] + random.choice(['@', '#', '$', '&']) + password[3 + 1:]
+        password = password[:3] + random.choice(['@', '#', '$', '&']) + password[3:]
+        password = adjust_password_length(password)
     print(f"password 3={password}")
     # (Только для Zoom (zoom=True) Два последних символа - два маленькие буквы,
-    # так удобнее потом дописывать 55
+    # так визуально удобнее потом дописывать 55
     if zoom:
         print(f"пароль для zoom")
         password = password + random.choice(ascii_lowercase_chars) + random.choice(ascii_lowercase_chars)
@@ -63,5 +64,22 @@ def random_password(strong=False, long=10, zoom=False):
     return password
 
 
+def adjust_password_length(pswd):
+    """
+    Выкидываем из пароля начиная с 1 символа (0 всегда должна быть буква) первую попавшуюся букву,
+    за счёт этого длина пароля при добавлении цифры или спецсимвола не увеличивается,
+    и цифры\спецсимволы не затираются.
+    :param pswd: Пароль на входе
+    :return: Нормализованный пароль
+    """
+    seq = range(1, len(pswd), 1)
+    for i in seq:
+        if pswd[i].isalpha():
+            # print(pswd[0:i] + pswd[i + 1:])
+            return pswd[0:i] + pswd[i + 1:]
+
+
+
 if __name__ == "__main__":
     print(random_password(strong=True, zoom=True))  # для ДШ
+    # adjust_password_length('xpt4LkWS')

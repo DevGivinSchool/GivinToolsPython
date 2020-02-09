dd8f8c420563574ea734388bcbb864df0db043d0
MinistrBob <ministrbob777@gmail.com>
2020-02-09 17:53:37 +0300

    ADD: Добавил строку с DEBUG

--- gtp_config.py
+++ gtp_config.py
@@ -8,4 +8,5 @@ config = dict(
     # Логирование производится в папку где лежит скрипт/log
     log_dir=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log'),
     log_level=logging.INFO
+    #log_level=logging.DEBUG
 )

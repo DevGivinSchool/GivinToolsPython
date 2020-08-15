ac6573038a488caf499d85468516ed4d40f91ee1
MinistrBob <ministrbob777@gmail.com>
2020-08-15 16:48:23 +0300

    save model Participant

--- gtp_index/models.py
+++ gtp_index/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 
@@ -13,3 +14,39 @@ class Article(models.Model):
     class Meta:
         verbose_name = "Статью"
         verbose_name_plural = "Статьи"
+
+
+class Participant(models.Model):
+    PARTICIPANT_TYPE_CHOICES = [
+        ('N', 'новый участник (первый месяц)'),
+        ('P', 'регулярный участник'),
+        ('B', 'заблокированный'),
+        ('E', 'наш сотрудник'),
+        ('V', 'VIP (особые условия, например, участие без оплаты)'),
+    ]
+    last_name = models.CharField(max_length=2000, verbose_name="Фамилия по русски"),
+    first_name = models.CharField(max_length=2000, verbose_name="Имя по русски"),
+    fio = models.CharField(max_length=4000, verbose_name="Фамилия и Имя по русски"),
+    email = models.CharField(max_length=254, verbose_name="Email"),
+    telegram = models.CharField(max_length=32, verbose_name="Telegram name"),
+    time_begin = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания"),
+    time_end = models.DateTimeField(verbose_name="Дата блокировки"),
+    login = models.CharField(max_length=2000,
+                             verbose_name="Логин пользователя в домене @givinschool.org, соответствует почте"),
+    password = models.CharField(max_length=30, verbose_name="Пароль участника"),
+    payment_date = models.DateField(default=datetime.date.today, verbose_name="Дата оплаты"),
+    number_of_days = models.PositiveSmallIntegerField(verbose_name="Количество оплаченных дней"),
+    deadline = models.DateField(verbose_name="Дата следующей оплаты"),
+    comment = models.CharField(max_length=4000, verbose_name="Комментарий"),
+    until_date = models.DateField(verbose_name="Дата до которой отсрочен платёж"),
+    type = models.CharField(
+        max_length=1,
+        choices=PARTICIPANT_TYPE_CHOICES,
+        default=N,
+        verbose_name="Тип участника: P - регулярный участник; B - заблокированный; E - наш сотрудник; V - VIP (особые условия, например, участие без оплаты)",
+    )
+    last_name_eng = models.CharField(max_length=2000, verbose_name="Фамилия по английски"),
+    first_name_eng = models.CharField(max_length=2000, verbose_name="Имя по английски"),
+    fio_eng = models.CharField(max_length=4000, verbose_name="Фамилия и Имя по английски"),
+    date_until_date = models.DateField(verbose_name="Дата выдачи отсрочки"),
+    telegram_id = models.BigIntegerField(verbose_name="Telegram id")

0524f0111eea0d068da14c944ab5374026063f69
MinistrBob <ministrbob777@gmail.com>
2020-08-23 10:56:12 +0300

    Работа над моделью

--- gtp_index/models.py
+++ gtp_index/models.py
@@ -16,6 +16,8 @@ class Article(models.Model):
         verbose_name_plural = "Статьи"
 
 
+# time_begin default=datetime.date.today
+# create_date = models.DateTimeField(default=timezone.now)
 class Participant(models.Model):
     PARTICIPANT_TYPE_CHOICES = [
         ('N', 'новый участник (первый месяц)'),
@@ -24,16 +26,16 @@ class Participant(models.Model):
         ('E', 'наш сотрудник'),
         ('V', 'VIP (особые условия, например, участие без оплаты)'),
     ]
-    last_name = models.CharField(null=True, max_length=2000, verbose_name="Фамилия по русски")
-    first_name = models.CharField(null=True, max_length=2000, verbose_name="Имя по русски")
-    fio = models.CharField(db_index=True, null=True, max_length=4000, verbose_name="Фамилия и Имя по русски")
+    last_name = models.CharField(null=True, max_length=255, verbose_name="Фамилия по русски")
+    first_name = models.CharField(null=True, max_length=255, verbose_name="Имя по русски")
+    fio = models.CharField(db_index=True, null=True, max_length=500, verbose_name="Фамилия и Имя по русски")
     email = models.CharField(db_index=True, null=True, max_length=254, verbose_name="Email")
     telegram = models.CharField(db_index=True, null=True, max_length=32, verbose_name="Telegram name")
-    time_begin = models.DateTimeField(editable=False, default=datetime.date.today, verbose_name="Дата создания")
+    time_begin = models.DateTimeField(null=False, editable=False, auto_now_add=True, verbose_name="Дата создания")
     time_end = models.DateTimeField(null=True, verbose_name="Дата блокировки")
-    login = models.CharField(null=True, max_length=2000,
+    login = models.CharField(null=True, max_length=32,
                              verbose_name="Логин пользователя в домене @givinschool.org, соответствует почте")
-    password = models.CharField(null=True, max_length=30, verbose_name="Пароль участника")
+    password = models.CharField(null=True, max_length=32, verbose_name="Пароль участника")
     payment_date = models.DateField(null=True, default=datetime.date.today, verbose_name="Дата оплаты")
     number_of_days = models.PositiveSmallIntegerField(null=True, verbose_name="Количество оплаченных дней")
     deadline = models.DateField(null=True, verbose_name="Дата следующей оплаты")
@@ -45,8 +47,15 @@ class Participant(models.Model):
         default='N',
         verbose_name="Тип участника: P - регулярный участник; B - заблокированный; E - наш сотрудник; V - VIP (особые условия, например, участие без оплаты)",
     )
-    last_name_eng = models.CharField(null=True, max_length=2000, verbose_name="Фамилия по английски")
-    first_name_eng = models.CharField(null=True, max_length=2000, verbose_name="Имя по английски")
-    fio_eng = models.CharField(db_index=True, null=True, max_length=4000, verbose_name="Фамилия и Имя по английски")
+    last_name_eng = models.CharField(null=True, max_length=255, verbose_name="Фамилия по английски")
+    first_name_eng = models.CharField(null=True, max_length=255, verbose_name="Имя по английски")
+    fio_eng = models.CharField(db_index=True, null=True, max_length=500, verbose_name="Фамилия и Имя по английски")
     date_until_date = models.DateField(null=True, verbose_name="Дата выдачи отсрочки")
     telegram_id = models.BigIntegerField(null=True, verbose_name="Telegram id")
+
+    def __str__(self):
+        return self.fio
+
+    class Meta:
+        verbose_name = "Участника"
+        verbose_name_plural = "Участников"

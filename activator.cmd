247833b0ce87ed8e8be4d8e331a390d2bba5c3e0
MinistrBob <ministrbob777@gmail.com>
2020-09-11 15:55:22 +0300

    Update activator.cmd

--- activator.cmd
+++ activator.cmd
@@ -1,2 +1,1 @@
-cd c:\MyGit\GivinToolsPython\
-c:\Users\MinistrBob\.virtualenvs\GivinToolsPython\Scripts\activate
+C:\Windows\System32\cmd.exe /k "cd c:\MyGit\GivinToolsPython\&c:\Users\MinistrBob\.virtualenvs\GivinToolsPython\Scripts\activate"

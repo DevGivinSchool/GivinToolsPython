c31f40c1b228bbeecbdbdab7ec7820db33460292
MinistrBob <ministrbob777@gmail.com>
2020-09-22 14:15:46 +0300

    Create password.cmd

---
+++ password.cmd
@@ -0,0 +1,1 @@
+C:\Windows\System32\cmd.exe /k "cd c:\MyGit\GivinToolsPython\ & c:\Users\MinistrBob\.virtualenvs\GivinToolsPython\Scripts\python.exe c:\MyGit\GivinToolsPython\core\password_generator_for_sf.py"

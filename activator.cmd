a8fe99b963fb811c2dac7f33627fe85b008c9707
MinistrBob <ministrbob777@gmail.com>
2020-11-16 17:26:58 +0300

    Небольшой рефакторинг

--- activator.cmd
+++
@@ -1,1 +0,0 @@
-C:\Windows\System32\cmd.exe /k "cd c:\MyGit\GivinToolsPython\&c:\Users\MinistrBob\.virtualenvs\GivinToolsPython\Scripts\activate"
--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path = "c:\MyGit\GivinToolsPython\sf\sf_main.py"
-t1 = os.path.dirname(os.path.dirname(path))
-print(t1)
-t2 = os.path.relpath(t1, start=os.curdir)
-print(t2)
